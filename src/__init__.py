886daa55ff9c8047007b2dded785d68ff500c1a5
iElectro <ejose.acc@gmail.com>
2022-02-17 10:03:35 -0500

    Quitado el cors

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,6 @@ from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
     if test_config is None:
 
         app.config.from_mapping(
